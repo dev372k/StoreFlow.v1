31701ef56e8424bf7dcc9d2faf1d8868e93a36d3
S.M.Owais <smowais.neduet@gmail.com>
2024-04-19 14:41:42 +0500

    Code refactoring 101

---
+++ DAL/Repositories/Implementations/Repository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories.Implementations
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public Repository()
+        {
+
+        }
+
+        public IEnumerable<TEntity> Search(object parameters, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model> Search<Model>(object parameters, string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Search(object parameters, string query);
+        IEnumerable<Model> Search<Model>(object parameters, string query);
+    }
+}
